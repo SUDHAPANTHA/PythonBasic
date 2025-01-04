ba9a43196c07489b0e1b5c9ea058703068f471a7
Sudha Pantha <sudhapantha111@gmail.com>
2025-01-04 20:13:29 +0545

    addition for int and float

---
+++ Lesson1/var.py
@@ -0,0 +1,11 @@
+#variables and Type practices
+#for int,float and decimal
+myint = 7
+print(myint)
+myfloat = 8.0
+print(myfloat)
+#sum of var
+myint = 7
+myfloat = 9.2
+sum = myint+myfloat
+print(sum)
