fd9775a57723f31af617a7ead231f46f69ff0c6b
SUDHAPANTHA <sudhapantha111@gmail.com>
2025-01-19 20:47:33 +0545

    views.py

--- book_app/views.py
+++ book_app/views.py
@@ -1,18 +1,19 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from book_app.models import Book
 
 def book_list(request):
     books = Book.objects.all()
     return render(request,"book_list.html",{"books": books})
-def add_book(request):
-    if request.method == "GET":
-        return render(request,"add_book.html")
+def book_create(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('book-list')  # Redirect to book list page
     else:
-        title = request.POST["title"]
-        book = Book(title=title)
-        book.save
-        return HttpResponseRedirect("/")
+        form = BookForm()
+    return render(request, 'book_app/book_form.html', {'form': form})
 def update_book(request, id):
             book = Book.objects.get(id=id)
             if request.method == "GET": 
